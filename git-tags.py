6ef0a0bf361d4c9dba859a0d694e468614fb7b20
Jason R. Coombs <jaraco@jaraco.com>
2016-09-10 14:26:08 -0400

    Dedent the main behavior, eliminate null branch.

--- git-tags.py
+++ git-tags.py
@@ -8,17 +8,18 @@ from mercurial import commands, extensions, scmutil
 
 
 def wrapper(orig, ui, repo, *names, **opts):
-    if opts.get('git'):
-        rev = opts.get('rev', '.')
-        rev = scmutil.revsingle(repo, rev).hex()
-        opener = repo.opener('git-tags', 'a+', atomictemp=True)
-        # Mercurial 3.8 compat
-        opener = contextlib.closing(opener)
-        with opener as file:
-            for name in names:
-                file.write("%s %s\n" % (rev, name))
-    else:
+    if not opts.get('git'):
         orig(ui, repo, *names, **opts)
+        return
+
+    rev = opts.get('rev', '.')
+    rev = scmutil.revsingle(repo, rev).hex()
+    opener = repo.opener('git-tags', 'a+', atomictemp=True)
+    # Mercurial 3.8 compat
+    opener = contextlib.closing(opener)
+    with opener as file:
+        for name in names:
+            file.write("%s %s\n" % (rev, name))
 
 extensions.wrapcommand(commands.table, 'tag', wrapper)
 
